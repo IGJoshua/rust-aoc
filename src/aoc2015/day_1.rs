d2a98d0b7b89f85da19eebe3cbea2fb35bddfc17
Joshua Suskalo <joshua@suskalo.org>
2021-11-08 22:20:42 -0600

    Move day 1 challenges to iterators

--- src/aoc2015/day_1.rs
+++ src/aoc2015/day_1.rs
@@ -1,71 +1,58 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
-
-pub fn part_1() -> Result<i32> {
-    let f = File::open("res/2015/day_1.txt")?;
-
-    let mut floor = 0;
-    for line in BufReader::new(f).lines() {
-        if let Ok(line) = line {
-            for c in line.chars() {
-                floor += match c {
-                    '(' => 1,
-                    ')' => -1,
-                    _ => 0,
-                }
-            }
-        } else {
-            panic!("The reader failed");
-        }
-    }
-    Ok(floor)
+pub fn part_1(input: &str) -> i64 {
+    input.lines()
+        .flat_map(|line| line.chars())
+        .map(|c| match c {
+            '(' => 1,
+            ')' => -1,
+            _ => 0,
+        }).sum()
 }
 
-pub fn part_2() -> Result<i32> {
-    let f = File::open("res/2015/day_1.txt")?;
-
+pub fn part_2(input: &str) -> usize {
     let mut floor = 0;
     let mut idx = 0;
-    'outer: for line in BufReader::new(f).lines() {
-        if let Ok(line) = line {
-            for c in line.chars() {
-                idx += 1;
+    'outer: for line in input.lines() {
+        for c in line.chars() {
+            idx += 1;
 
-                floor += match c {
-                    '(' => 1,
-                    ')' => -1,
-                    _ => 0,
-                };
+            floor += match c {
+                '(' => 1,
+                ')' => -1,
+                _ => 0,
+            };
 
-                if floor < 0 {
-                    break 'outer;
-                }
+            if floor < 0 {
+                break 'outer;
             }
         }
     }
 
-    Ok(idx)
+    idx
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::fs::File;
+    use std::io::Read;
 
     #[test]
     fn test_part_1() {
-        if let Ok(result) = part_1() {
-            assert_eq!(result, 232, "the result was correct floor");
-        } else {
-            assert!(false, "a file error occurred");
-        }
+        let mut f = File::open("res/2015/day_1.txt").unwrap();
+        let mut input = String::new();
+        f.read_to_string(&mut input).unwrap();
+
+        let result = part_1(&input);
+        assert_eq!(result, 232, "the result was correct floor");
     }
 
     #[test]
     fn test_part_2() {
-        if let Ok(index) = part_2() {
-            assert_eq!(index, 1783, "the result was correct instruction");
-        } else {
-            assert!(false, "a file error occurred");
-        }
+        let mut f = File::open("res/2015/day_1.txt").unwrap();
+        let mut input = String::new();
+        f.read_to_string(&mut input).unwrap();
+
+        let index = part_2(&input);
+        assert_eq!(index, 1783, "the result was correct instruction");
     }
 }
